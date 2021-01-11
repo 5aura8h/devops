57858191745ba792137da0b9f6059b6f4d7da25c
5aura8h <44212751+5aura8h@users.noreply.github.com>
2021-01-11 21:54:30 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html>
+<center>
+<h1> Sandbox Web Server for Testing </h1>
+
+<body style="background-color:powderblue;">
+
+<pre>
+<?php
+print "\n";
+print "Container IP: ";
+print `hostname -I`;
+print "Container Hostname: ";
+print `hostname`;
+?>
+</pre>
+
+</center>
+</body>
+</html>
