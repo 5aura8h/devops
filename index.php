aa134ce87f811e6c43fcdc57e74a4778823a07df
5aura8h <kotharisaurabh11@gmail.com>
2021-01-12 15:02:00 +0530

    updated

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <html>
 <center>
 <h1> Sandbox Web Server for Testing </h1>
@@ -7,14 +6,21 @@
 
 <pre>
 <?php
+
 print "\n";
-print "Container IP: ";
-print `hostname -I`;
-print "Container Hostname: ";
-print `hostname`;
+
+$host = gethostname();
+$b = '<h2 style="color:Tomato;">' . gethostbyname($host) . '</h2>';
+
+$a = '<h2>' . "Container IP: " . '</h2>';
+
+echo $a.$b; 
+
+
 ?>
 </pre>
 
 </center>
 </body>
 </html>
+
